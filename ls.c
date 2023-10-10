97f0cac7733eef19880aa0af5d3325074cb237f0
iadekunle3 <iadekunle34@gmail.com>
2023-10-10 15:38:55 -0700

    push

--- ls.c
+++ ls.c
@@ -1,18 +1,34 @@
+#include "shell.h"
 #include <unistd.h>
 #include <stdio.h>
 /**
  * ls_exe - is used to call ls function
  * @args: is the double pointer to the args
+ * Return: as specified
  * Author: Nnon and Isaac
  */
 void ls_exe(char **args)
 {
     char *envp[] = { NULL };
+    pid_t pid = fork();
 
-    if (execve("/bin/ls", args, envp) == -1)
+    if (pid < 0)
     {
-        perror("execve");
+        perror("fork");
+        return;
+    }
+
+    if (pid == 0)
+    {
+        
+        if (execve("/bin/ls", args, envp) == -1)
+        {
+            perror("execve");
+            exit(EXIT_FAILURE);
+        }
+    }
+    else
+    {
+        wait(NULL);
     }
 }
-
-
