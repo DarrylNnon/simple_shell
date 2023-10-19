b8ebc547118278c6c7e1eef2fa7dcb1c0b6c4c12
iadekunle3 <iadekunle34@gmail.com>
2023-10-19 05:07:51 -0700

    check

--- cd.c
+++ cd.c
@@ -1,55 +1,79 @@
 #include "shell.h"
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
 
 /**
-  * _cd - change directory
-  * @argv: argument vector
-  * @env: environment linked list
-  */
-int _cd(char **argv, env_list_t **env)
+ * cd_command - change the current working directory based on the provided arguments
+ * @args: An array of arguments for the cd command.
+ * Authors: Nnon & Isaac
+ */
+void cd_command(char **args)
 {
-	char buf[BUFSIZ];
-	char *home = _getenv_list_value("HOME", env);
-	char *old = _getenv_list_value("OLDPWD", env);
-	char **OLDPWD = malloc(sizeof(char *) * 4);
-	char **PWD = malloc(sizeof(char *) * 4);
-	DIR *dir;
+    char *home_dir;
+    char old_dir[256];
+    char new_dir[256];
 
-	OLDPWD[0] = "";
-	OLDPWD[1] = "OLDPWD";
-	OLDPWD[2] = buf;
-	OLDPWD[3] = NULL;
-	PWD[0] = "";
-	PWD[1] = "PWD";
-	PWD[2] = buf;
-	PWD[3] = NULL;
-	getcwd(buf, sizeof(buf));
+    if (getcwd(old_dir, sizeof(old_dir)) == NULL)
+    {
+        perror("getcwd");
+        return;
+    }
 
-	getcwd(old_dir, sizeof(old_dir));
-	if (args[1] == NULL || strcmp(args[1], "~") == 0)
-	{
-		home_dir = getenv("HOME");
-		if (home_dir != NULL)
-		{
-			chdir(home_dir);
-		}
-		else
-		{
-			printu("cd: HOME not set\n");
-		}
-	}
-	else if (strcmp(args[1], "-") == 0)
-	{
-		chdir(getenv("OLDPWD"));
-	}
-	else
-	{
-		if (chdir(args[1]) != 0)
-		{
-			perror("cd");
-		}
-	}
-	getcwd(new_dir, sizeof(new_dir));
-	setenv("PWD", new_dir, 1);
-	setenv("OLDPWD", old_dir, 1);
+    if (args[1] == NULL || strcmp(args[1], "~") == 0)
+    {
+        home_dir = getenv("HOME");
+        if (home_dir != NULL)
+        {
+            if (chdir(home_dir) != 0)
+            {
+                perror("cd");
+                return;
+            }
+        }
+        else
+        {
+            printf("cd: HOME not set\n");
+            return;
+        }
+    }
+    else if (strcmp(args[1], "-") == 0)
+    {
+        char *oldpwd = getenv("OLDPWD");
+        if (oldpwd != NULL)
+        {
+            if (chdir(oldpwd) != 0)
+            {
+                perror("cd");
+                return;
+            }
+        }
+        else
+        {
+            printf("cd: OLDPWD not set\n");
+            return;
+        }
+    }
+    else
+    {
+        if (chdir(args[1]) != 0)
+        {
+            perror("cd");
+            return;
+        }
+    }
+
+    if (getcwd(new_dir, sizeof(new_dir)) == NULL)
+    {
+        perror("getcwd");
+        return;
+    }
+
+    if (setenv("PWD", new_dir, 1) != 0 || setenv("OLDPWD", old_dir, 1) != 0)
+    {
+        perror("setenv");
+        return;
+    }
 }
 
